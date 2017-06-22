2b71928be316fbe845ca7ba357be8af3fbecb0e1
Prasanna <prasanna.bajracharya@gmail.com>
2017-06-22 09:53:21 -0500

    Orders added.

--- src/main/java/edu/mum/coffee/ViewController/ProductViewController.java
+++ src/main/java/edu/mum/coffee/ViewController/ProductViewController.java
@@ -1,14 +1,23 @@
 package edu.mum.coffee.ViewController;
 
+import edu.mum.coffee.DTO.OrderLineDTO;
+import edu.mum.coffee.domain.Order;
+import edu.mum.coffee.domain.Orderline;
+import edu.mum.coffee.domain.Person;
 import edu.mum.coffee.domain.Product;
+import edu.mum.coffee.service.OrderService;
+import edu.mum.coffee.service.PersonService;
 import edu.mum.coffee.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.jws.WebParam;
+import java.security.Principal;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,10 +28,41 @@ public class ProductViewController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/productList")
     public String productList(Model model){
 
         model.addAttribute("products", productService.getAllProduct());
         return "productList";
+    }
+
+    @PostMapping("/orderNow")
+
+    public String saveOrder(OrderLineDTO orderLineDTO, Principal principal) {
+        Order order = new Order();
+        order.setOrderDate(new Date());
+        Person person = personService.findByEmail(principal.getName()).get(0);
+        order.setPerson(person);
+
+        Orderline orderline = new Orderline();
+        orderline.setQuantity(orderLineDTO.getQuantity());
+        orderline.setProduct(productService.getProduct(orderLineDTO.getProductId()));
+        order.addOrderLine(orderline);
+
+        orderService.save(order);
+
+        return "redirect:/getOrderList";
+
+    }
+
+    @GetMapping("/getOrderList")
+    public String getOrderList(Model model,Principal principal){
+        model.addAttribute("orders",orderService.findByPerson(personService.findByEmail(principal.getName()).get(0)));
+        return "orderList";
     }
 }
